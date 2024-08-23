50f9feebc83db49bf19e31e666ca1b097c335edd
Kelvin Anigboro <ranigboro@gmail.com>
2024-08-24 00:08:26 +0100

    Implement generation of pre-signed URLs for each file

--- src/listCSVsHandler.ts
+++ src/listCSVsHandler.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  ListObjectsV2Command,
+  GetObjectCommand
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import logger from './utils/logger';
 
 const s3Client = new S3Client({});
@@ -27,9 +32,26 @@ export const listCSVsHandler = async (
     const data = await s3Client.send(command);
     const files = data.Contents || [];
 
+    logger.info(`Found ${files.length} files.`);
+
+    const fileUrls = await Promise.all(
+      files.map(async (file) => {
+        const getObjectParams = {
+          Bucket: bucketName,
+          Key: file.Key!
+        };
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+        return {
+          fileName: file.Key,
+          url: url
+        };
+      })
+    );
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ files: files.map((file) => file.Key) })
+      body: JSON.stringify({ files: fileUrls })
     };
   } catch (error) {
     logger.error('Error in listCSVsHandler:', error);
